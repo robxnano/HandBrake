c734909bcb7eb390f150365aaaed6459540ed228
robxnano <89391914+robxnano@users.noreply.github.com>
2025-09-17 20:28:35 +0100

    build: Update CMake files

--- make/cmake/cmake_uninstall.cmake
+++ make/cmake/cmake_uninstall.cmake
@@ -5,17 +5,18 @@ endif()
 file(READ "${CMAKE_CURRENT_BINARY_DIR}/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 foreach(file ${files})
-  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
   if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    message(STATUS "Uninstalling: $ENV{DESTDIR}${file}")
     execute_process(
-      COMMAND ${CMAKE_COMMAND} -E remove "$ENV{DESTDIR}${file}"
+      COMMAND ${CMAKE_COMMAND} -E rm "$ENV{DESTDIR}${file}"
       OUTPUT_VARIABLE rm_out
+      ERROR_VARIABLE  rm_err
       RESULT_VARIABLE rm_retval
-      )
+    )
     if(NOT "${rm_retval}" STREQUAL 0)
-      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
+      message(FATAL_ERROR "${rm_err}")
     endif()
-  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
-    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  else()
+    message(STATUS "File does not exist: $ENV{DESTDIR}${file}")
   endif()
 endforeach()
